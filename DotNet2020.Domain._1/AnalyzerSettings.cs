c531f817babbf3002b792e4707f88d05b4727ec1
Dmitry Chegodaev <cheg15032001@gmail.com>
2020-05-28 00:55:49 +0300

    select ef

---
+++ DotNet2020.Domain._1/AnalyzerSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet2020.Domain._1
+{
+    class AnalyzerSettings
+    {
+        public int MaxParamsCount { get; set; }
+        public int MaxLineLength { get; set; }
+        public int MaxMethodLength { get; set; }
+
+        /// <summary>Максимально допустимое число методов в классе</summary>
+        public int MaxClassСapacity { get; set; }
+        public string Language { get; set; }
+    }
+}
--- DotNet2020.Domain._1/Stylistic/LineLengthAnalyzer.cs
+++ DotNet2020.Domain._1/Stylistic/LineLengthAnalyzer.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
+using Newtonsoft.Json;
 using System;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,18 +16,17 @@ namespace DotNet2020.Domain._1
     {
         public const string DiagnosticId = "LineLengthDiagnosticId";
         public const string CodeFixTitle = "make the line shorter";
-        const int MaxLength = 80;
         const string Title = "line is too long";
         //static string mesFormat = $"line should be shorter than {MaxLength} symbols";
         const string MessageFormat = "line should be shorter than 80 symbols";
         const string Category = "Stylistic";
         const string Description = "line should be shorter than 80 symbols";
+        const int MaxLength = 80;
         public static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
         public static void AnalyzeTree(SyntaxTreeAnalysisContext context)
         {
             var tree = context.Tree;
-
             var lines = tree.GetText().Lines;
             foreach (var l in lines)
             {
